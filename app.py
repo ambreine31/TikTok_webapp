ad1bf8baf9716d59c2b3f69150617b26b36fe496
ambreine31 <bourdierambre@gmail.com>
2020-07-05 06:02:58 +0200

    deployment works only sometimes

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ from TikTokApi import TikTokApi
 import pickle
 import time
 from selenium import webdriver
+import os
 api = TikTokApi()
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+
 
 #driver = webdriver.Chrome('/chromedriver') 
 class User:
@@ -100,7 +110,7 @@ def login():
         if(option == "user"):
             username = request.form['username']
             print(username)
-            toks = api.userLikedbyUsername(username, count=200)
+            toks = api.userLikedbyUsername(username, count=100)
             Get_TikToks(toks)
             session['user_id'] = 1
             return redirect(url_for('level1'))
